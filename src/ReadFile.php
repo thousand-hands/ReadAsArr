1451eb8243853e2339d21bde96db6f26a5e14df8
none <none@none.com>
2021-05-17 10:16:59 +0800

    update

--- src/ReadFile.php
+++ src/ReadFile.php
@@ -4,6 +4,10 @@ namespace ReadAsArr;
 
 class ReadFile
 {
+    /**
+     * @param $fileName
+     * @return \Generator
+     */
     public static function asArr($fileName): \Generator
     {
         if (file_exists($fileName) && $handle = fopen($fileName, 'rb')) {
